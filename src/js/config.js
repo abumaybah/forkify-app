f61e2fa007769418a9e325df0311341acd4647aa
abumaybah <abdulazizmahmudov01@gmail.com>
2022-08-08 13:35:11 +0500

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c95acd7c-21cd-4fd1-947d-f931726ff818';
+export const MODAL_CLOSE_SEC = 2.5;
